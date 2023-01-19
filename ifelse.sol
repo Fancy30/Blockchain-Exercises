2dd4b17627c8bccd90d0632580d4b8b2f652aa61
Anu Adekoya <105782815+Fancy30@users.noreply.github.com>
2023-01-19 11:36:56 +0100

    Create ifelse.sol

---
+++ ifelse.sol
@@ -0,0 +1,13 @@
+if(_x == true){
+            _x = paused;
+        }else{
+            _x = unpaused;
+        }
+
+          function setPauseAndUnpause(bool _x) public view  {
+        if(_x == true){
+            _x = paused;
+        }else{
+            _x = unpaused;
+        }
+    }
