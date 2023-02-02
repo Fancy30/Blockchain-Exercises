8df867a655e063033b6cd987a25c1abc0c719a0c
Anu Adekoya <105782815+Fancy30@users.noreply.github.com>
2023-02-02 12:28:37 +0100

    Create if.sol

---
+++ if.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract tech4dev{
+
+    function Uzoh(uint _x) public pure returns(uint){
+
+        //if and else statements
+
+    if(_x < 10){
+
+        return 1;
+
+    }
+
+    }  
+
+/*Classwork Create a function that accept input, 
+then create an if statement that return 2 
+if number inputed is less than 20.
+*/
+    function Moz(uint _x) public pure returns(uint) {
+
+        //if and else statements
+
+    if(_x < 20) {
+
+        return 2;
+
+    } else {
+
+        return 4;
+    }
+
+    }  
+   
+}
