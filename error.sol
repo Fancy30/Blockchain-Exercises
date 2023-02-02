86f3623efd598e3683e40745626b761f2d1f25ad
Anu Adekoya <105782815+Fancy30@users.noreply.github.com>
2023-02-02 13:00:53 +0100

    require

---
+++ error.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract tech4dev {
+
+//REQUIRE
+
+    function Felicia(uint _x) public pure {
+
+        require(_x < 10, "It is above than 10");
+    }
+}
