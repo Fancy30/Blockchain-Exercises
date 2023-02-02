34504faa93d8613dea046c8a179e5d63dffc645b
Anu Adekoya <105782815+Fancy30@users.noreply.github.com>
2023-02-02 12:10:12 +0100

    beginner

---
+++ struct.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract dare{
+
+    struct Winner{
+
+        uint age;
+        string name;
+    }
+
+    uint[] public mike = [1, 2, 3];
+
+    //struct-name[] array-name;
+
+    Winner[] vick;
+
+}
