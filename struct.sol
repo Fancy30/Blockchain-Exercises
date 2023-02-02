5f799d602e044d44db4dbb9a1c96138151346029
Anu Adekoya <105782815+Fancy30@users.noreply.github.com>
2023-02-02 12:21:04 +0100

    Update struct.sol

--- struct.sol
+++ struct.sol
@@ -16,3 +16,23 @@ contract dare{
     Winner[] vick;
 
 }
+
+// another sample
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract sandwichFactory{
+
+    struct Sandwich{
+        string name;
+        uint age;
+    }
+
+    Sandwich[] public sandwiches;
+
+    function michael(uint _index) public{
+        Sandwich storage Bonolo = sandwiches[_index];
+    }
+   
+}
